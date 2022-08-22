f1b228c8d4a47f0c692bce26f6d77bc446dc8167
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-22 18:31:28 +0800

    Add css

--- pet-clinic/src/App.jsx
+++ pet-clinic/src/App.jsx
@@ -1,5 +1,15 @@
+import NavBar from "./components/NavBar";
+import AnimalType from "./views/AnimalType";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="h-screen">
+      <NavBar />
+      <div className="body">
+        <AnimalType />
+      </div>
+    </div>
+  );
 }
 
 export default App;
