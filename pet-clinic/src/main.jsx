001d29600370ba6541661ce5c9c73737de8b792f
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-24 14:09:46 +0800

    Add Animal view

--- pet-clinic/src/main.jsx
+++ pet-clinic/src/main.jsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AnimalType from "./views/AnimalType";
 import AddAnimalType from "./views/AddAnimalType";
 import EditAnimalType from "./views/EditAnimalType";
+import Animal from "./views/Animal/Animal";
+import AddAnimal from "./views/Animal/AddAnimal";
+import EditAnimal from "./views/Animal/EditAnimal";
 import CONST from "../src/constants/index";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -20,6 +23,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route
           path={`${CONST.ROUTE.ANIMAL_TYPE}/:id/${CONST.ROUTE.EDIT}`}
           element={<EditAnimalType />}
+        />
+        <Route path={CONST.ROUTE.ANIMAL} element={<Animal />} />
+        <Route
+          path={`${CONST.ROUTE.ANIMAL}/${CONST.ROUTE.ADD}`}
+          element={<AddAnimal />}
+        />
+        <Route
+          path={`${CONST.ROUTE.ANIMAL}/:id/${CONST.ROUTE.EDIT}`}
+          element={<EditAnimal />}
         />
       </Route>
     </Routes>
