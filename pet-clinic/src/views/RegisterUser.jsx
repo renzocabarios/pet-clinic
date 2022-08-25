e08f0ee32581a60840f0224e3e7f1517353d49ee
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-25 11:07:06 +0800

    Add RegisterUser in views

---
+++ pet-clinic/src/views/RegisterUser.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import api from "../services/api.service";
+import CONST from "../constants/index";
+import { useNavigate } from "react-router-dom";
+import FormInput from "../components/FormInput";
+
+function RegisterUser() {
+  const navigate = useNavigate();
+
+  const [formdata, setformdata] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+
+  const submit = async () => {
+    await api.post(CONST.ROUTE.USER, formdata);
+    navigate(`/`);
+  };
+
+  const inputs = [
+    {
+      name: "email",
+      title: "Email",
+      onChange: (e) => {
+        setformdata((prevState) => ({
+          ...prevState,
+          email: e.target.value,
+        }));
+      },
+    },
+    {
+      name: "password",
+      title: "Password",
+      onChange: (e) => {
+        setformdata((prevState) => ({
+          ...prevState,
+          password: e.target.value,
+        }));
+      },
+    },
+    {
+      name: "firstName",
+      title: "First Name",
+      onChange: (e) => {
+        setformdata((prevState) => ({
+          ...prevState,
+          firstName: e.target.value,
+        }));
+      },
+    },
+    {
+      name: "lastName",
+      title: "Last Name",
+      onChange: (e) => {
+        setformdata((prevState) => ({
+          ...prevState,
+          lastName: e.target.value,
+        }));
+      },
+    },
+  ];
+
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <div className="shadow-md p-10 flex flex-col items-center">
+        <h1>Create User</h1>
+        {inputs.map((i) => {
+          return (
+            <FormInput
+              name={i.name}
+              title={i.title}
+              onChange={i.onChange}
+              key={i.name}
+            />
+          );
+        })}
+        <button className="shadow-md p-2" onClick={submit}>
+          Create Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterUser;
