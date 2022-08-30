cfb69f8707eb1f22ecc8f5c07dd7586bf0ea6ed2
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-30 09:32:44 +0800

    Add Position view

---
+++ pet-clinic/src/views/Position/Position.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useEffect } from "react";
+import DataTable from "../../components/DataTable";
+import { useNavigate } from "react-router-dom";
+import CONST from "../../constants/index";
+import PrimaryButton from "../../components/PrimaryButton";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData, deleteData } from "../../states/reducers/position.reducer";
+
+function Position() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const data = useSelector((state) => {
+    return state.positionReducer.entries;
+  });
+
+  const deleteById = async (id) => {
+    dispatch(deleteData({ id }));
+  };
+
+  const updateById = async (id) => {
+    navigate(`${id}/${CONST.ROUTE.EDIT}`);
+  };
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [data]);
+
+  return (
+    <>
+      <PrimaryButton
+        title={"Add"}
+        onClick={() => {
+          navigate(CONST.ROUTE.ADD);
+        }}
+      />
+      <DataTable
+        header={["Position Name", "Description"]}
+        data={data}
+        dataName={["name", "description"]}
+        deleteById={deleteById}
+        updateById={updateById}
+      />
+    </>
+  );
+}
+
+export default Position;
