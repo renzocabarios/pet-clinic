5f141d25684ed3d504b1586593cdc5f188499f83
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-31 10:49:56 +0800

    Import animalReducer in states

--- pet-clinic/src/states/index.js
+++ pet-clinic/src/states/index.js
@@ -4,6 +4,7 @@ import authReducer from "./reducers/auth.reducer";
 import positionReducer from "./reducers/position.reducer";
 import personnelReducer from "./reducers/personnel.reducer";
 import animalTypeReducer from "./reducers/animal-type.reducer";
+import animalReducer from "./reducers/animal.reducer";
 
 export const store = configureStore({
   reducer: {
@@ -12,5 +13,6 @@ export const store = configureStore({
     positionReducer,
     personnelReducer,
     animalTypeReducer,
+    animalReducer,
   },
 });
