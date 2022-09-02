733318827932a81344ce817d11b3d95d51c239e0
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-02 14:54:21 +0800

    Barrel states

--- pet-clinic/src/states/index.js
+++ pet-clinic/src/states/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import diseaseReducer from "./reducers/disease.reducer";
-import authReducer from "./reducers/auth.reducer";
-import positionReducer from "./reducers/position.reducer";
-import personnelReducer from "./reducers/personnel.reducer";
-import animalTypeReducer from "./reducers/animal-type.reducer";
-import animalReducer from "./reducers/animal.reducer";
+import {
+  diseaseReducer,
+  authReducer,
+  positionReducer,
+  personnelReducer,
+  animalTypeReducer,
+  animalReducer,
+} from "./reducers";
 
 export const store = configureStore({
   reducer: {
