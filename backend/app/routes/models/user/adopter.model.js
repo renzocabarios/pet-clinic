04e414a0821ebf6b83a767df40540e8192e6f0ae
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-05 23:48:10 +0800

    Update user models options

--- backend/app/routes/models/user/adopter.model.js
+++ backend/app/routes/models/user/adopter.model.js
@@ -3,25 +3,14 @@ import paginate from "mongoose-paginate";
 import CONST from "../../../constants/index.js";
 import model from "./user.model.js";
 
-const options = {
-  timestamps: {
-    createdAt: "DateCreated",
-    updatedAt: "DateUpdated",
-  },
-  discriminatorKey: "type",
-};
-
-const schema = mongoose.Schema(
-  {
-    animals: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: CONST.MODEL.ANIMAL,
-      },
-    ],
-  },
-  options
-);
+const schema = mongoose.Schema({
+  animals: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: CONST.MODEL.ANIMAL,
+    },
+  ],
+});
 
 schema.plugin(paginate);
 
--- backend/app/routes/models/user/personnel.model.js
+++ backend/app/routes/models/user/personnel.model.js
@@ -3,25 +3,14 @@ import paginate from "mongoose-paginate";
 import CONST from "../../../constants/index.js";
 import model from "./user.model.js";
 
-const options = {
-  timestamps: {
-    createdAt: "DateCreated",
-    updatedAt: "DateUpdated",
+const schema = mongoose.Schema({
+  position: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: CONST.MODEL.POSITION,
+    required: [true, "Personnel Position is required."],
   },
-  discriminatorKey: "type",
-};
-
-const schema = mongoose.Schema(
-  {
-    position: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: CONST.MODEL.POSITION,
-      required: [true, "Personnel Position is required."],
-    },
-  },
-  options
-);
+});
 
 schema.plugin(paginate);
 
-export default model.discriminator(CONST.MODEL.USER.ADOPTER, schema);
+export default model.discriminator(CONST.MODEL.USER.PERSONNEL, schema);
