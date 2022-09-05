e31e0ca93ed9097dd4b7abf903cc7727133013c4
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-05 15:20:39 +0800

    Update redux store

--- pet-clinic/src/main.jsx
+++ pet-clinic/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./states/index";
+import { store } from "./states/store";
 import { Router } from "./components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
